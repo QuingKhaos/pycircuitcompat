46799cec1958a3748060ff2069cee6b00db2e405
Em Karisch <em@khaos.solutions>
2025-09-16 14:14:37 +0200

    feat: integrate heat-sensor (#17)

---
+++ data-updates/heat-sensor.lua
@@ -0,0 +1,13 @@
+if mods["heat-sensor"] then
+  -- Better subgroup placement
+  data.raw.item["heat-sensor"].subgroup = "circuit-input"
+
+  -- Require battery in recipe as other vanilla circuit network entities do in pyanodons
+  if mods["pyalternativeenergy"] then
+    table.insert(data.raw.recipe["heat-sensor"].ingredients, {type = "item", name = "battery-mk01", amount = 1})
+  end
+
+  -- Unlock with advanced-material-processing-2 technology
+  data.raw.recipe["heat-sensor"].enabled = false
+  table.insert(data.raw.technology["advanced-material-processing-2"].effects, {type = "unlock-recipe", recipe = "heat-sensor"})
+end

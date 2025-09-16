40551c5c4d9c348a5616d29093c96f05c719fe06
Em Karisch <em@khaos.solutions>
2025-09-16 12:07:44 +0200

    fix(compaktcircuit): use same science packs as advanced-combinators

--- data-final-fixes/compaktcircuit.lua
+++ data-final-fixes/compaktcircuit.lua
@@ -1,5 +1,5 @@
 if mods["compaktcircuit"] then
-  -- Remove advanced circuit and processing unit from recipe, py is hard enough already
+  -- Remove advanced circuit from recipe, py is hard enough already
   for _, recipe in pairs({"compaktcircuit-processor", "compaktcircuit-processor_1x1"}) do
     for _, ingredient_to_remove in pairs({"advanced-circuit"}) do
       local index = nil
@@ -20,7 +20,7 @@ if mods["compaktcircuit"] then
     end
   end
 
-  -- Make tech dependent on circuit network to make it available earlier and use the same science packs
+  -- Make compaktcircuit-tech technology dependent on circuit-network technology to make it available earlier and use the same science packs as advanced-combinators technology
   data.raw.technology["compaktcircuit-tech"].prerequisites = {"circuit-network"}
-  data.raw.technology["compaktcircuit-tech"].unit = data.raw.technology["circuit-network"].unit
+  data.raw.technology["compaktcircuit-tech"].unit = data.raw.technology["advanced-combinators"].unit
 end

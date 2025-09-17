2f506b283a694589aa41ed5bc90eaace204322ed
Em Karisch <em@khaos.solutions>
2025-09-17 16:17:02 +0200

    refactor: extract generic tweaks to beltcounter2-tweaks

--- data-updates/beltcounter2.lua
+++ data-updates/beltcounter2.lua
@@ -1,13 +1,17 @@
 if mods["beltcounter2"] then
-  -- Better subgroup placement
-  data.raw.item["belt-arithmetic-combinator"].subgroup = "circuit-input"
+  if not mods["beltcounter2-tweaks"] then
+    -- Better subgroup placement
+    data.raw.item["belt-arithmetic-combinator"].subgroup = "circuit-input"
+  end
 
-  -- Replace power switch with arithmetic combinator, py is hard enough already
-  for i = 1, #data.raw.recipe["belt-arithmetic-combinator"].ingredients do
-    ingredients = data.raw.recipe["belt-arithmetic-combinator"].ingredients[i]
-    if ingredients.name == "power-switch" then
-      data.raw.recipe["belt-arithmetic-combinator"].ingredients[i].name = "arithmetic-combinator"
-      break
+  if not mods["beltcounter2-tweaks"] then
+    -- Replace power switch with arithmetic combinator, py is hard enough already
+    for i = 1, #data.raw.recipe["belt-arithmetic-combinator"].ingredients do
+      ingredients = data.raw.recipe["belt-arithmetic-combinator"].ingredients[i]
+      if ingredients.name == "power-switch" then
+        data.raw.recipe["belt-arithmetic-combinator"].ingredients[i].name = "arithmetic-combinator"
+        break
+      end
     end
   end
 end

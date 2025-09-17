0e974e28cc055f6612bbb1b03445185f18ce02f5
Em Karisch <em@khaos.solutions>
2025-09-18 00:10:35 +0200

    refactor: extract generic tweaks to Cybersyn-Content-Reader-Tweaks mod

--- data-updates/Cybersyn-Content-Reader.lua
+++ data-updates/Cybersyn-Content-Reader.lua
@@ -1,8 +1,10 @@
 if mods["Cybersyn-Content-Reader"] then
-  -- Better subgroup placement
-  data.raw.item["cybersyn-provider-reader"].subgroup = "circuit-input"
-  data.raw.item["cybersyn-requester-reader"].subgroup = "circuit-input"
-  data.raw.item["cybersyn-delivery-reader"].subgroup = "circuit-input"
+  if not mods["Cybersyn-Content-Reader-Tweaks"] then
+    -- Better subgroup placement
+    data.raw.item["cybersyn-provider-reader"].subgroup = "circuit-input"
+    data.raw.item["cybersyn-requester-reader"].subgroup = "circuit-input"
+    data.raw.item["cybersyn-delivery-reader"].subgroup = "circuit-input"
+  end
 
   -- Require battery in recipe as other vanilla circuit network entities do in pyanodons
   if mods["pyalternativeenergy"] then
@@ -11,14 +13,16 @@ if mods["Cybersyn-Content-Reader"] then
     table.insert(data.raw.recipe["cybersyn-delivery-reader"].ingredients, {type = "item", name = "battery-mk01", amount = 1})
   end
 
-  -- Remove circuit-network-2 technology and move unlocks to cybersyn-train-network technology
-  if data.raw.technology["circuit-network-2"] then
-    data.raw.technology["circuit-network-2"] = nil
-  end
+  if not mods["Cybersyn-Content-Reader-Tweaks"] then
+    -- Remove circuit-network-2 technology and move unlocks to cybersyn-train-network technology
+    if data.raw.technology["circuit-network-2"] then
+      data.raw.technology["circuit-network-2"] = nil
+    end
 
-  if data.raw.technology["cybersyn-train-network"] then
-    table.insert(data.raw.technology["cybersyn-train-network"].effects, { type = "unlock-recipe", recipe = "cybersyn-provider-reader" })
-    table.insert(data.raw.technology["cybersyn-train-network"].effects, { type = "unlock-recipe", recipe = "cybersyn-requester-reader" })
-    table.insert(data.raw.technology["cybersyn-train-network"].effects, { type = "unlock-recipe", recipe = "cybersyn-delivery-reader" })
+    if data.raw.technology["cybersyn-train-network"] then
+      table.insert(data.raw.technology["cybersyn-train-network"].effects, {type = "unlock-recipe", recipe = "cybersyn-provider-reader"})
+      table.insert(data.raw.technology["cybersyn-train-network"].effects, {type = "unlock-recipe", recipe = "cybersyn-requester-reader"})
+      table.insert(data.raw.technology["cybersyn-train-network"].effects, {type = "unlock-recipe", recipe = "cybersyn-delivery-reader"})
+    end
   end
 end

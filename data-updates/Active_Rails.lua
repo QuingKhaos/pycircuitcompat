d2fb0e69271cc83ec8f9a764267ba498c22fc868
Em Karisch <em@khaos.solutions>
2025-09-17 14:04:30 +0200

    refactor: extract generic tweaks to Active_Rails_Tweaks

--- data-updates/Active_Rails.lua
+++ data-updates/Active_Rails.lua
@@ -1,14 +1,18 @@
 if mods["Active_Rails"] then
-  -- Better subgroup placement
-  data.raw["rail-planner"]["active-rail"].subgroup = "circuit-input"
-  data.raw.item["rail-combinator"].subgroup = "circuit-input"
+  if not mods["Active_Rails_Tweaks"] then
+    -- Better subgroup placement
+    data.raw["rail-planner"]["active-rail"].subgroup = "circuit-input"
+    data.raw.item["rail-combinator"].subgroup = "circuit-input"
+  end
 
   -- Require battery in recipe as other vanilla circuit network entities do in pyanodons
   if mods["pyalternativeenergy"] then
     table.insert(data.raw.recipe["active-rail"].ingredients, {type = "item", name = "battery-mk01", amount = 1})
   end
 
-  -- Unlock with automated rail transportation technology
-  data.raw.recipe["active-rail"].enabled = false
-  table.insert(data.raw.technology["automated-rail-transportation"].effects, {type = "unlock-recipe", recipe = "active-rail"})
+  if not mods["Active_Rails_Tweaks"] then
+    -- Unlock with automated rail transportation technology
+    data.raw.recipe["active-rail"].enabled = false
+    table.insert(data.raw.technology["automated-rail-transportation"].effects, {type = "unlock-recipe", recipe = "active-rail"})
+  end
 end

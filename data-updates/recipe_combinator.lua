e196576dab919628f2d11fe1288de7f406857fc5
Em Karisch <em@khaos.solutions>
2025-09-18 12:22:37 +0200

    refactor: extract generic tweaks to recipe_combinator_tweaks mod (#73)

--- data-updates/recipe_combinator.lua
+++ data-updates/recipe_combinator.lua
@@ -1,4 +1,6 @@
 if mods["recipe_combinator"] then
-  -- Better subgroup placement
-  data.raw.item["recipe-combinator-main"].subgroup = "circuit-combinator"
+  if not mods["recipe_combinator_tweaks"] then
+    -- Better subgroup placement
+    data.raw.item["recipe-combinator-main"].subgroup = "circuit-combinator"
+  end
 end

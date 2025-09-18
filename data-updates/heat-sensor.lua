74fb986df0e51be84e16c44a4ae23d3ef9e8ad5e
Em Karisch <em@khaos.solutions>
2025-09-18 09:20:51 +0200

    refactor: extract generic tweaks to heat-sensor-tweaks mod

--- data-updates/heat-sensor.lua
+++ data-updates/heat-sensor.lua
@@ -1,13 +1,17 @@
 if mods["heat-sensor"] then
-  -- Better subgroup placement
-  data.raw.item["heat-sensor"].subgroup = "circuit-input"
+  if not mods["heat-sensor-tweaks"] then
+    -- Better subgroup placement
+    data.raw.item["heat-sensor"].subgroup = "circuit-input"
+  end
 
   -- Require battery in recipe as other vanilla circuit network entities do in pyanodons
   if mods["pyalternativeenergy"] then
     table.insert(data.raw.recipe["heat-sensor"].ingredients, {type = "item", name = "battery-mk01", amount = 1})
   end
 
-  -- Unlock with advanced-material-processing-2 technology
-  data.raw.recipe["heat-sensor"].enabled = false
-  table.insert(data.raw.technology["advanced-material-processing-2"].effects, {type = "unlock-recipe", recipe = "heat-sensor"})
+  if not mods["heat-sensor-tweaks"] then
+    -- Unlock with advanced-material-processing-2 technology
+    data.raw.recipe["heat-sensor"].enabled = false
+    table.insert(data.raw.technology["advanced-material-processing-2"].effects, {type = "unlock-recipe", recipe = "heat-sensor"})
+  end
 end

3bd46d6cf1edf0ea766247231a55744df44d62aa
Em Karisch <em@khaos.solutions>
2025-09-19 11:10:02 +0200

    refactor: extract generic tweaks to spoilage-scanner-tweaks mod

--- data-updates/spoilage-scanner.lua
+++ data-updates/spoilage-scanner.lua
@@ -1,4 +1,6 @@
 if mods["spoilage-scanner"] then
-  -- Better subgroup placement
-  data.raw.item["spoilage-scanner"].subgroup = "circuit-input"
+  if not mods["spoilage-scanner-tweaks"] then
+    -- Better subgroup placement
+    data.raw.item["spoilage-scanner"].subgroup = "circuit-input"
+  end
 end

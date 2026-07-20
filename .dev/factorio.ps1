317e1b7826abbfeec79d5d4a38cc7556699e45d9
Em Karisch <emii.khaos@gmail.com>
2026-07-20 09:25:02 +0200

    chore: updates from mod template

--- .dev/factorio.ps1
+++ .dev/factorio.ps1
@@ -2,6 +2,8 @@ Set-Variable -Name workspace -Value $Env:FACTORIO_WORKSPACE
 Set-Variable -Name mod_base -Value $Env:FACTORIO_STEAM_MODS_BASE
 Set-Variable -Name package -Value "pycircuitcompat"
 
+New-Item -ItemType Directory -Force -Path "$mod_base\$package" | Out-Null
+
 Set-Variable -Name version -Value (Get-Content "$workspace\$package\info.json" | ConvertFrom-Json).version
 Move-Item -Path "$workspace\$package\${package}_${version}.zip" -Destination "${mod_base}\${package}" -Force -ErrorAction SilentlyContinue
 

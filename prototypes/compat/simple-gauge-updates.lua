d31c7cb30b730d93d910dfda777150cc5d2b1292
Em Karisch <emii.khaos@gmail.com>
2026-07-21 16:00:18 +0200

    fix: crash without simple gauge

--- prototypes/compat/simple-gauge-updates.lua
+++ prototypes/compat/simple-gauge-updates.lua
@@ -1,5 +1,7 @@
-local create_from_pipe_prototype = require("__simple-gauge__.scripts.create_from_pipe_prototype")
 local khaoslib_item = require("__khaoslib__.prototypes.item")
+
+if mods["simple-gauge"] then
+  local create_from_pipe_prototype = require("__simple-gauge__.scripts.create_from_pipe_prototype")
 
   if mods["pyindustry"] then
     create_from_pipe_prototype("niobium-pipe", mods["pycoalprocessing"] and "niobium" or "py-storage-tanks")
@@ -10,3 +12,4 @@ local khaoslib_item = require("__khaoslib__.prototypes.item")
     create_from_pipe_prototype("ht-pipes", "coal-processing-3")
     khaoslib_item:load("simple-gauge-ht-pipes"):set {subgroup = "circuit-input"} :commit()
   end
+end
